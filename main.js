6ff14c2c573f78e51908181e80ebfc56c33e1535
utkarshbisht16 <utkarshbisht116@gmail.com>
2022-02-11 00:42:38 +0530

    implemented file download and upload

--- main.js
+++ main.js
@@ -99,53 +99,61 @@ btnfile.addEventListener("click",
             rid: rid,
             rname: rname,
             rtype: "text-file",
-            pid: cfid
+            pid: cfid,
+            isBold: false,
+            isItalic: false,
+            isUnderline: false,
+            BGcolor: "#000000",
+            FGcolor: "#FFFFFF",
+            fontFamily: "serif",
+            fontSize: 22,
+            content: "Hi, write here..."
 
         });
         //Storage save
         saveToStorage();
     });
 function addTextFileHTML(rname, rid, pid) {
-        // taking template and making a clone of it.
-        let divTextFileTemplate = template.content.querySelector(".text-file");
-        let divTextFile = document.importNode(divTextFileTemplate, true); // makes a copy
-    
-        let spanrename = divTextFile.querySelector("[action=Rename]");
-        let spandelete = divTextFile.querySelector("[action=Delete]");
-        let spanview = divTextFile.querySelector("[action=View]");
-    
-        //setting the name of folder
-        let divName = divTextFile.querySelector("[purpose=name]");
-        divName.innerHTML = rname;
-        divTextFile.setAttribute("pid", pid);
-        divTextFile.setAttribute("rid", rid);
-    
-        spanrename.addEventListener("click", renameTextFile);
-        spandelete.addEventListener("click", deleteTextFile);
-        spanview.addEventListener("click", viewTextFile);
-    
-        divContainer.appendChild(divTextFile);
+    // taking template and making a clone of it.
+    let divTextFileTemplate = template.content.querySelector(".text-file");
+    let divTextFile = document.importNode(divTextFileTemplate, true); // makes a copy
+
+    let spanrename = divTextFile.querySelector("[action=Rename]");
+    let spandelete = divTextFile.querySelector("[action=Delete]");
+    let spanview = divTextFile.querySelector("[action=View]");
+
+    //setting the name of folder
+    let divName = divTextFile.querySelector("[purpose=name]");
+    divName.innerHTML = rname;
+    divTextFile.setAttribute("pid", pid);
+    divTextFile.setAttribute("rid", rid);
+
+    spanrename.addEventListener("click", renameTextFile);
+    spandelete.addEventListener("click", deleteTextFile);
+    spanview.addEventListener("click", viewTextFile);
+
+    divContainer.appendChild(divTextFile);
 }
 
-aRoothPath.addEventListener("click",viewFromBreadcrum);
+aRoothPath.addEventListener("click", viewFromBreadcrum);
 
-function viewFromBreadcrum(){
+function viewFromBreadcrum() {
     let apath = this;
     let fid = parseInt(apath.getAttribute("rid"));
 
     // set breadcrum
-    while(apath.nextSibling){
+    while (apath.nextSibling) {
         apath.parentNode.removeChild(apath.nextSibling);
     }
 
     // set the container
     cfid = fid;
     divContainer.innerHTML = "";
-    for(let i = 0; i < Resource.length; i++){
+    for (let i = 0; i < Resource.length; i++) {
         if (Resource[i].pid == cfid) { // only show folders which pid = cfid
-            if(Resource[i].rtype == "folder"){
+            if (Resource[i].rtype == "folder") {
                 addfolderHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
-            }else if(Resource[i].rtype == "text-file"){
+            } else if (Resource[i].rtype == "text-file") {
                 addTextFileHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
             }
         }
@@ -161,10 +169,10 @@ function deleteFolder() {
     let FidTBD = divFolder.getAttribute("rid");
 
     let sure = confirm(`Are you sure that you want to delete ${fname} ?`);
-    if(!sure){
+    if (!sure) {
         return;
     }
-    
+
     //delete from HTML
     divContainer.removeChild(divFolder);
 
@@ -173,15 +181,15 @@ function deleteFolder() {
     //delete from storage
     saveToStorage();
 }
-function deleteHelper(FidTBD){
-    let Children = Resource.filter(r=> r.pid == FidTBD);
-    for(let i = 0; i < Children.length; i++){
+function deleteHelper(FidTBD) {
+    let Children = Resource.filter(r => r.pid == FidTBD);
+    for (let i = 0; i < Children.length; i++) {
         //assume that it will delete child and their children recursively
         deleteHelper(Children[i].rid);
     }
-    
+
     let ridx = Resource.findIndex(r => r.rid == FidTBD);
-    Resource.splice(ridx,1);
+    Resource.splice(ridx, 1);
 }
 
 function deleteTextFile() {
@@ -193,16 +201,16 @@ function deleteTextFile() {
     let FidTBD = divTextFile.getAttribute("rid");
 
     let sure = confirm(`Are you sure that you want to delete ${fname} ?`);
-    if(!sure){
+    if (!sure) {
         return;
     }
-    
+
     //delete from HTML
     divContainer.removeChild(divTextFile);
 
     // delete from RAM
     let ridx = Resource.findIndex(r => r.rid == FidTBD);
-    Resource.splice(ridx,1);
+    Resource.splice(ridx, 1);
     //delete from storage
     saveToStorage();
 }
@@ -215,20 +223,20 @@ function viewFolder() {
     let fid = parseInt(divFolder.getAttribute("rid"));
 
     let aPathTemplate = template.content.querySelector("a[purpose='path']");
-    let apath = document.importNode(aPathTemplate,true);
+    let apath = document.importNode(aPathTemplate, true);
 
     apath.innerHTML = fname;
-    apath.setAttribute("rid",fid);
-    apath.addEventListener("click",viewFromBreadcrum);
+    apath.setAttribute("rid", fid);
+    apath.addEventListener("click", viewFromBreadcrum);
     divBreadcrum.appendChild(apath);
 
     cfid = fid;
     divContainer.innerHTML = "";
-    for(let i = 0; i < Resource.length; i++){
+    for (let i = 0; i < Resource.length; i++) {
         if (Resource[i].pid == cfid) { // only show folders which pid = cfid
-            if(Resource[i].rtype == "folder"){
+            if (Resource[i].rtype == "folder") {
                 addfolderHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
-            }else if(Resource[i].rtype == "text-file"){
+            } else if (Resource[i].rtype == "text-file") {
                 addTextFileHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
             }
         }
@@ -242,17 +250,17 @@ function viewTextFile() {
     let fid = parseInt(divFile.getAttribute("rid"));
 
     let divNotepadMenuTemp = template.content.querySelector("[purpose=notepad-menu]");
-    let divNotepadMenu = document.importNode(divNotepadMenuTemp,true);
+    let divNotepadMenu = document.importNode(divNotepadMenuTemp, true);
     divAppMenuBar.innerHTML = "";
     divAppMenuBar.appendChild(divNotepadMenu);
 
     let divNotepadBodyTemp = template.content.querySelector("[purpose=notepad-body]");
-    let divNotepadBody = document.importNode(divNotepadBodyTemp,true);
+    let divNotepadBody = document.importNode(divNotepadBodyTemp, true);
     divAppBody.innerHTML = "";
     divAppBody.appendChild(divNotepadBody);
 
-    divAppTitle.innerHTML=fname;
-    divAppTitle.setAttribute("rid",fid);
+    divAppTitle.innerHTML = fname;
+    divAppTitle.setAttribute("rid", fid);
 
     let spansave = divAppMenuBar.querySelector("[action=save]");
     let spanbold = divAppMenuBar.querySelector("[action=bold]");
@@ -262,22 +270,26 @@ function viewTextFile() {
     let inputFGcolour = divAppMenuBar.querySelector("[action=fg-color]");
     let selectFontFamily = divAppMenuBar.querySelector("[action=font-family]");
     let selectFontSize = divAppMenuBar.querySelector("[action=font-size]");
+    let spanDownload = divAppMenuBar.querySelector("[action=download]");
+    let inputUpload = divAppMenuBar.querySelector("[action=upload]");
     let textarea = divAppBody.querySelector("textArea");
 
-    
-    spansave.addEventListener("click",saveNotepad); 
-    spanbold.addEventListener("click",makeNotepadBold) 
-    spanitalic.addEventListener("click",makeNotepadItalic)
-    spanunderline.addEventListener("click",makeNotepadUnderline) 
-    inputBGcolour.addEventListener("change",ChangeNotepadBGcolour) 
-    inputFGcolour.addEventListener("change",ChangeNotepadFGcolour)
-    selectFontFamily.addEventListener("change",ChangeNotepadFontFamily) 
-    selectFontSize.addEventListener("change",ChangeNotepadFontSize) 
 
-    let resource = Resource.find(r=> r.rid == fid);
+    spansave.addEventListener("click", saveNotepad);
+    spanbold.addEventListener("click", makeNotepadBold)
+    spanitalic.addEventListener("click", makeNotepadItalic)
+    spanunderline.addEventListener("click", makeNotepadUnderline)
+    inputBGcolour.addEventListener("change", ChangeNotepadBGcolour)
+    inputFGcolour.addEventListener("change", ChangeNotepadFGcolour)
+    selectFontFamily.addEventListener("change", ChangeNotepadFontFamily)
+    selectFontSize.addEventListener("change", ChangeNotepadFontSize)
+    spanDownload.addEventListener("click", downloadNotepad)
+    inputUpload.addEventListener("change", uploadNotepad)
+
+    let resource = Resource.find(r => r.rid == fid);
     spanbold.setAttribute("pressed", !resource.isBold)
     spanitalic.setAttribute("pressed", !resource.isItalic)
-    spanunderline.setAttribute("pressed", !resource.isUnderline) 
+    spanunderline.setAttribute("pressed", !resource.isUnderline)
     inputBGcolour.value = resource.BGcolor;
     inputFGcolour.value = resource.FGcolor;
     selectFontFamily.value = resource.fontFamily;
@@ -291,14 +303,14 @@ function viewTextFile() {
     inputFGcolour.dispatchEvent(new Event("change"))
     selectFontFamily.dispatchEvent(new Event("change"))
     selectFontSize.dispatchEvent(new Event("change"))
-    
+
 
 
 }
 
-function saveNotepad(){
+function saveNotepad() {
     let fid = parseInt(divAppTitle.getAttribute("rid"));
-    let resource = Resource.find(r=> r.rid == fid);
+    let resource = Resource.find(r => r.rid == fid);
 
     let spanbold = divAppMenuBar.querySelector("[action=bold]");
     let spanitalic = divAppMenuBar.querySelector("[action=italic]");
@@ -317,61 +329,118 @@ function saveNotepad(){
     resource.fontFamily = selectFontFamily.value;
     resource.fontSize = selectFontSize.value;
     resource.content = textarea.value;
-    saveToStorage();        
+    saveToStorage();
 
 }
-function makeNotepadBold(){
+function makeNotepadBold() {
     let textArea = divAppBody.querySelector("textArea");
     let isPressed = this.getAttribute("pressed") == "true";
-    if(isPressed == false){
-        this.setAttribute("pressed",true);
-        textArea.style.fontWeight="bold";
-    }else{
-        this.setAttribute("pressed",false);
-        textArea.style.fontWeight="normal";
+    if (isPressed == false) {
+        this.setAttribute("pressed", true);
+        textArea.style.fontWeight = "bold";
+    } else {
+        this.setAttribute("pressed", false);
+        textArea.style.fontWeight = "normal";
     }
 }
-function makeNotepadItalic(){
+function makeNotepadItalic() {
     let textArea = divAppBody.querySelector("textArea");
     let isPressed = this.getAttribute("pressed") == "true";
-    if(isPressed == false){
-        this.setAttribute("pressed",true);
-        textArea.style.fontStyle="italic";
-    }else{
-        this.setAttribute("pressed",false);
-        textArea.style.fontStyle="normal";
+    if (isPressed == false) {
+        this.setAttribute("pressed", true);
+        textArea.style.fontStyle = "italic";
+    } else {
+        this.setAttribute("pressed", false);
+        textArea.style.fontStyle = "normal";
     }
 }
-function makeNotepadUnderline(){
+function makeNotepadUnderline() {
     let textArea = divAppBody.querySelector("textArea");
     let isPressed = this.getAttribute("pressed") == "true";
-    if(isPressed == false){
-        this.setAttribute("pressed",true);
-        textArea.style.textDecoration="underline";
-    }else{
-        this.setAttribute("pressed",false);
-        textArea.style.textDecoration="none";
+    if (isPressed == false) {
+        this.setAttribute("pressed", true);
+        textArea.style.textDecoration = "underline";
+    } else {
+        this.setAttribute("pressed", false);
+        textArea.style.textDecoration = "none";
     }
 }
-function ChangeNotepadBGcolour(){
+function ChangeNotepadBGcolour() {
     let colour = this.value;
     let textArea = divAppBody.querySelector("textArea");
     textArea.style.background = colour;
 }
-function ChangeNotepadFGcolour(){
+function ChangeNotepadFGcolour() {
     let colour = this.value;
     let textArea = divAppBody.querySelector("textArea");
     textArea.style.color = colour;
 }
-function ChangeNotepadFontFamily(){
+function ChangeNotepadFontFamily() {
     let fontFamily = this.value;
     let textArea = divAppBody.querySelector("textArea");
     textArea.style.fontFamily = fontFamily;
 }
-function ChangeNotepadFontSize(){
+function ChangeNotepadFontSize() {
     let fontSize = this.value;
     let textArea = divAppBody.querySelector("textArea");
     textArea.style.fontSize = fontSize;
+}
+function uploadNotepad() {
+    let file = window.event.target.files[0];
+    let reader = new FileReader();
+    reader.addEventListener("load",
+        function upload() {
+            let data = window.event.target.result;
+            //creating obj. of data
+            let resource = JSON.parse(data);
+
+            let spanbold = divAppMenuBar.querySelector("[action=bold]");
+            let spanitalic = divAppMenuBar.querySelector("[action=italic]");
+            let spanunderline = divAppMenuBar.querySelector("[action=underline]");
+            let inputBGcolour = divAppMenuBar.querySelector("[action=bg-color]");
+            let inputFGcolour = divAppMenuBar.querySelector("[action=fg-color]");
+            let selectFontFamily = divAppMenuBar.querySelector("[action=font-family]");
+            let selectFontSize = divAppMenuBar.querySelector("[action=font-size]");
+            let textarea = divAppBody.querySelector("textArea")
+
+            spanbold.setAttribute("pressed", !resource.isBold)
+            spanitalic.setAttribute("pressed", !resource.isItalic)
+            spanunderline.setAttribute("pressed", !resource.isUnderline)
+            inputBGcolour.value = resource.BGcolor;
+            inputFGcolour.value = resource.FGcolor;
+            selectFontFamily.value = resource.fontFamily;
+            selectFontSize.value = resource.fontSize;
+            textarea.value = resource.content;
+
+            spanbold.dispatchEvent(new Event("click"))
+            spanitalic.dispatchEvent(new Event("click"))
+            spanunderline.dispatchEvent(new Event("click"))
+            inputBGcolour.dispatchEvent(new Event("change"))
+            inputFGcolour.dispatchEvent(new Event("change"))
+            selectFontFamily.dispatchEvent(new Event("change"))
+            selectFontSize.dispatchEvent(new Event("change"))
+            // viewTextFile();
+        })
+    reader.readAsText(file);
+}
+function downloadNotepad() {
+    let fid = parseInt(divAppTitle.getAttribute("rid"));
+    let resource = Resource.find(r => r.rid == fid);
+    let divNotepadMenu = this.parentNode;
+
+    let setToDownload = JSON.stringify(resource); // download in string formt
+    let encodedData = encodeURIComponent(setToDownload); //downloaded file have some secret char so they need to be encoded before using
+
+    let adownload = divNotepadMenu.querySelector("a[purpose=download]")
+    // json command for download operation
+    // adownload.setAttribute("href", "data:text/json; charset=utf-8," + JSON.stringify(resource)); -> without encoding it will download half of the data only
+
+    adownload.setAttribute("href", "data:," + encodedData);
+    // setting download attribute
+    // download attribute is used to decided the name of file after the download.
+    adownload.setAttribute("download", resource.rname + ".json");
+
+    adownload.click();
 }
 function renameFolder() {
     let Nrname = prompt("Enter a new name of folder.");
@@ -456,9 +525,9 @@ function loadFromStorage() {
     Resource = JSON.parse(rjson);
     for (let i = 0; i < Resource.length; i++) {
         if (Resource[i].pid == cfid) { // only show folders which pid = cfid
-            if(Resource[i].rtype == "folder"){
+            if (Resource[i].rtype == "folder") {
                 addfolderHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
-            }else if(Resource[i].rtype == "text-file"){
+            } else if (Resource[i].rtype == "text-file") {
                 addTextFileHTML(Resource[i].rname, Resource[i].rid, Resource[i].pid);
             }
         }
